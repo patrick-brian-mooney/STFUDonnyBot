49fae6b359e94ba9b7f22491ea60618449aacf9c
Patrick Brian Mooney <git.pmooney78@neverbox.com>
2018-01-08 16:46:58 -0700

    8 Jan 2017: working on reply code

--- STFUDonnyBot.py
+++ STFUDonnyBot.py
@@ -4,7 +4,7 @@
 don't apply to the accounts of world leaders, this bot tests whether those
 rules are also interpreted differently for people speaking back to bullying
 world leaders. In order to test this, it says something that, though perhaps
-not civil, is certainly something that needs to be said much more often. 
+not civil, is certainly something that needs to be said much more often.
 
 That is, STFUDonnyBot is a bot that waits for @realDonaldTrump to tweet, then
 replies with an image GIF of Walter Sobchek saying 'Shut the fuck up, Donny.'
@@ -14,8 +14,9 @@ GPL, either version 3 or (at your option) any later version. See the LICENSE
 file for details.
 """
 
-import json, pprint, random
+import json, pprint, random, sys, time
 
+import tweepy
 from tweepy.streaming import StreamListener                     # http://www.tweepy.org
 from tweepy import OAuthHandler
 from tweepy import Stream
@@ -24,17 +25,20 @@ from http.client import IncompleteRead
 import pid                                                      # https://pypi.python.org/pypi/pid/
 
 import patrick_logger                                           # https://github.com/patrick-brian-mooney/python-personal-library/
+from patrick_logger import log_it
 import social_media as sm                                       # https://github.com/patrick-brian-mooney/python-personal-library/
 from social_media_auth import Trump_client, Trump_client_for_personal_account   # Unshared module that contains my authentication constants     # FIXME
 
 consumer_key, consumer_secret = Trump_client['consumer_key'], Trump_client['consumer_secret']
 access_token, access_token_secret = Trump_client['access_token'], Trump_client['access_token_secret']
 
-target_accounts = ['realDonaldTrump']
+# target_accounts = ['realDonaldTrump']
+target_accounts = {'98912248': 'patrick_mooney'}
 image_URLs = ['https://media.giphy.com/media/cdxGGMh7UDuAE/giphy.gif',                                                  # while bowling
               'http://s2.quickmeme.com/img/1f/1fd3eb2d600dfff5506ff549f5fd58fc7949f4f56eea21a8ddd689b2a13eb1a5.jpg',    # out of your element
               'https://media1.tenor.com/images/98e1c9a96042b4ea6fae90b43ebc9fa5/tenor.gif?itemid=5761606',              # at the theater
               ]
+API = None      # This will be redefined as the Tweepy API object during startup.
 
 # OK, let's work around a problem that comes from the confluence of Debian's ancient packaging and rapid changes in Python's Requests package.
 try:
@@ -54,6 +58,10 @@ except Exception as e:              # If it's not defined, try to import it.
             log_it('WARNING: still got exception "%s"; defining by fiat instead' % e)
             ProtocolError = IncompleteRead
 
+def reply(original_tweet_data):
+    """#FIXME """
+    tweetId = original_tweet_data['id']
+    API.update_status('@%s %s' % (original_tweet_data['user']['id_str'], random.choice(image_URLs)),tweetId)
 
 class TrumpListener(StreamListener):
     """Of all the people on Twitter, Donald Trump is probably the least worth listening to. So let's
@@ -63,8 +71,8 @@ class TrumpListener(StreamListener):
         try:
             data = json.loads(data)
             try:
-                if data['user']['id_str'] in target_accounts:       # If it's the account we're watching, reply to it. 
-                    pass                                            # FIXME: actually reply
+                if data['user']['id_str'] in target_accounts:       # If it's the account we're watching, reply to it.
+                    reply(data)
             except KeyError:
                 log_it('INFO: we got minimal data again', 1)
                 log_it('... value of data is:\n\n%s\n\n' % pprint.pformat(data), 1)
@@ -80,10 +88,11 @@ class TrumpListener(StreamListener):
 
 if __name__ == '__main__':
     try:
-        with pid.PidFile(piddir=home_dir):
+        with pid.PidFile(piddir='/home/patrick/Documents/programming/python_projects/STFUDonnyBot'):
             l = TrumpListener()
             auth = OAuthHandler(consumer_key, consumer_secret)
             auth.set_access_token(access_token, access_token_secret)
+            API = tweepy.API(auth)
             log_it("... OK, we're set up, and about to watch %s" % ', '.join(target_accounts), 0)
             while True:
                 try:
